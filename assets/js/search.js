a82fb365b3d9de701bbd206d04beb0afb16f39cc
saomai <cumgar.dl47@gmail.com>
2024-10-25 00:28:57 +0700

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -78,7 +78,10 @@ $(document).ready(function() {
     function searchVideos() {
         const baseQuery = $search.val().trim();
         if (!baseQuery) return;
-
+        
+        $search.val(baseQuery);
+        $search.attr('value', baseQuery);
+        
         const query = currentTab !== 'TẤT CẢ' ? `${currentTab.toLowerCase()} ${baseQuery}` : baseQuery;
         const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=24&q=${query}&type=video&key=${apiKeys[currentApiKeyIndex]}`;
 
