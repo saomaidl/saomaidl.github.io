26286fbc30f94556559f6185da4c060e89f425ee
saomai <cumgar.dl47@gmail.com>
2024-10-24 22:20:56 +0700

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,7 +1,7 @@
-// firebase-config.js
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js';
-import { getAuth } from 'https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js';
-
+import { getAuth, signInAnonymously } from 'https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js';
+import { getFirestore, doc, setDoc, getDoc } from 'https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js';
+import { getDatabase } from 'https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js';
 const firebaseConfig = {
   apiKey: "AIzaSyBBPKJcXjkZ0wUbXNremQuf_R1Z6baYcU0",
   authDomain: "karaoke-3aa8f.firebaseapp.com",
@@ -11,10 +11,8 @@ const firebaseConfig = {
   messagingSenderId: "652741363415",
   appId: "1:652741363415:web:e33bb49a0be7eed7e09292"
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-
-// Export auth để sử dụng ở các file khác
-export { auth };
+const db = getFirestore(app);
+const realTimeDb = getDatabase(app);
+export { app, auth, db, realTimeDb };
