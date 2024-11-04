304eda69c0da5a016576572212e8a65afa7e34ee
saomai <cumgar.dl47@gmail.com>
2024-11-04 12:16:14 +0700

    Update sort.js

--- assets/js/sort.js
+++ assets/js/sort.js
@@ -30,43 +30,24 @@ setPersistence(auth, browserLocalPersistence)
 async function getAllUserIndexes(currentUserId) {
     try {
         const usersRef = ref(realTimeDb, 'users');
-
         onValue(usersRef, async (snapshot) => {
-            if (!snapshot.exists()) {
-                return;
-            }
+            if (!snapshot.exists()) return;
 
             const usersData = snapshot.val();
             const userIndexes = processAllUserData(usersData, currentUserId);
-            
             const songs = await getSongsFromFirestore();
 
-            customerData = userIndexes.map(user => {
-                const userId = user.uid;
-                const song = songs.find(s => s.id === userId);
-            
-                if (!song) {
-                    console.warn(`No song found for userId: ${userId}`);
-                }
-                return {
-                    customerId: userId,
-                    videoId: song ? song.videoId : null
-                };
-            });
+            customerData = userIndexes.map(user => ({
+                customerId: user.uid,
+                videoId: songs.find(s => s.id === user.uid)?.videoId || null
+            }));
 
             currentVideo = customerData[0];
             nextVideo = customerData[1];
-            
-            displaySongs(userIndexes, songs, currentUserId);
 
-            if (!isVideoPlayerInitialized) {
-                initializeVideoPlayer();
-                isVideoPlayerInitialized = true;
-            }
-            
-        }, (error) => {
-            console.error("Error reading user data:", error);
-        });
+            displaySongs(userIndexes, songs, currentUserId);
+            if (!isVideoPlayerInitialized) initializeVideoPlayer();
+        }, error => console.error("Error reading user data:", error));
     } catch (error) {
         console.error("Error getting user data:", error);
     }
@@ -87,46 +68,31 @@ async function handleEndOfVideo(userId) {
       viewCount: deleteField(),
       songSelected: false
     });
-    const userRealtimeRef = ref(realTimeDb, `users/${userId}`);
-    await remove(userRealtimeRef);
+    await remove(ref(realTimeDb, `users/${userId}`));
   } catch (error) {
-    console.error("Lỗi khi cập nhật Firestore hoặc Realtime Database:", error);
+    console.error("Error updating Firestore or Realtime Database:", error);
   }
 }
 
 function processAllUserData(usersData, currentUserId) {
-    const usersArray = Object.keys(usersData).map(key => ({
-        uid: key,
-        ...usersData[key]
-    }));
-
+    const usersArray = Object.keys(usersData).map(key => ({ uid: key, ...usersData[key] }));
     const filteredUsers = usersArray.filter(user => user.played === false);
 
     filteredUsers.sort((a, b) => {
-        const selectA = Number(a.select === true);
-        const selectB = Number(b.select === true);
-        if (selectA !== selectB) {
-            return selectB - selectA;
-        }
-        const priorityA = Number(a.priority === true);
-        const priorityB = Number(b.priority === true);
-        if (priorityA !== priorityB) {
-            return priorityB - priorityA;
-        }
+        if (a.select !== b.select) return Number(b.select) - Number(a.select);
+        if (a.priority !== b.priority) return Number(b.priority) - Number(a.priority);
         return a.timestamp - b.timestamp;
     });
+
     const result = filteredUsers.map((user, index) => ({
         ...user,
         index: index,
         isCurrentUser: user.uid === currentUserId
     }));
 
-    const allSelectFalse = result.every(user => user.select === false);
-
-    if (allSelectFalse && result.length > 0) {
+    if (result.every(user => user.select === false) && result.length > 0) {
         const firstUser = result[0];
-        const userRef = ref(realTimeDb, `users/${firstUser.uid}`);
-        update(userRef, { select: true })
+        update(ref(realTimeDb, `users/${firstUser.uid}`), { select: true })
             .then(() => console.log(`Updated select for user ${firstUser.uid} to true.`))
             .catch(error => console.error("Error updating select:", error));
         result[0].select = true;
@@ -138,13 +104,7 @@ function processAllUserData(usersData, currentUserId) {
 async function getSongsFromFirestore() {
     const songsCollection = collection(db, 'users');
     const songDocs = await getDocs(songsCollection);
-    
-    const songs = [];
-    songDocs.forEach(doc => {
-        songs.push({ id: doc.id, ...doc.data() });
-    });
-
-    return songs;
+    return songDocs.docs.map(doc => ({ id: doc.id, ...doc.data() }));
 }
 
 function displaySongs(userIndexes, songs, currentUserId) {
@@ -152,55 +112,33 @@ function displaySongs(userIndexes, songs, currentUserId) {
     playlistContainer.innerHTML = '';
 
     userIndexes.forEach((user, index) => {
-        const userId = user.uid;
-        const song = songs.find(s => s.id === userId);
-
+        const song = songs.find(s => s.id === user.uid);
         if (song) {
-            const [minutes, seconds] = song.duration.split(':');
-            const durationText = `${parseInt(minutes)} phút, ${parseInt(seconds)} giây`;
-
+            const durationText = `${parseInt(song.duration.split(':')[0])} phút, ${parseInt(song.duration.split(':')[1])} giây`;
             let backgroundColor = '';
-            if (index === 0 && userId !== currentUserId) {
-                backgroundColor = 'style="background-color: rgb(235, 222, 221);"';
-            } else if (index === 0 && userId === currentUserId) {
-                backgroundColor = '';
-            } else if (userId === currentUserId) {
-                backgroundColor = song.id !== userIndexes[0].uid ? 'style="background-color: rgb(221, 229, 235);"' : '';
-            }
-
-            const selectedClass = index === 0 ? 'ytm-playlist-panel-video-renderer-v2--selected' : '';
-            const ariaSelected = index === 0 ? 'true' : 'false';
+            if (index === 0 && user.uid !== currentUserId) backgroundColor = 'style="background-color: rgb(235, 222, 221);"';
+            if (index === 0 && user.uid === currentUserId) backgroundColor = '';
+            if (user.uid === currentUserId && song.id !== userIndexes[0].uid) backgroundColor = 'style="background-color: rgb(221, 229, 235);"';
 
             const songElement = `
-                <ytm-playlist-panel-video-renderer class="ytm-playlist-panel-video-renderer-v2 ${selectedClass}" aria-selected="${ariaSelected}" data-has-overflow-menu="false" ${backgroundColor} data-video-id="${song.videoId}"data-index="${index}"data-user-id="${userId}">
-                  <div class="compact-media-item" data-has-subscribe-button="" data-color-palette-applied="false">
-                    <a href="/songs?v=${song.videoId}" class="compact-media-item-image" aria-hidden="true">
-                      <ytm-compact-thumbnail class="video-thumbnail-container-compact center video-thumbnail-container-compact-rounded">
-                        <div class="cover video-thumbnail-img video-thumbnail-bg"></div>
-                        <img alt="" class="yt-core-image cover video-thumbnail-img yt-core-image--fill-parent-height yt-core-image--fill-parent-width yt-core-image--content-mode-scale-aspect-fill yt-core-image--loaded" src="${song.thumbnail}">
-                        <div class="video-preview-shim"></div>
-                        <div class="video-thumbnail-overlay-bottom-group">
-                          <ytm-thumbnail-overlay-time-status-renderer class="" data-style="DEFAULT">
-                            <badge-shape class="badge-shape-wiz badge-shape-wiz--thumbnail-default badge-shape-wiz--thumbnail-badge">
-                              <div class="badge-shape-wiz__text">${song.duration}</div>
-                            </badge-shape>
-                          </ytm-thumbnail-overlay-time-status-renderer>
+                <ytm-playlist-panel-video-renderer class="ytm-playlist-panel-video-renderer-v2 ${index === 0 ? 'ytm-playlist-panel-video-renderer-v2--selected' : ''}" aria-selected="${index === 0}" data-video-id="${song.videoId}" data-index="${index}" data-user-id="${user.uid}">
+                    <div class="compact-media-item">
+                        <a href="/songs?v=${song.videoId}" class="compact-media-item-image">
+                            <ytm-compact-thumbnail class="video-thumbnail-container-compact center video-thumbnail-container-compact-rounded">
+                                <div class="cover video-thumbnail-img video-thumbnail-bg"></div>
+                                <img alt="" class="yt-core-image cover video-thumbnail-img yt-core-image--fill-parent-height yt-core-image--fill-parent-width yt-core-image--content-mode-scale-aspect-fill yt-core-image--loaded" src="${song.thumbnail}">
+                                <div class="video-thumbnail-overlay-bottom-group">
+                                    <ytm-thumbnail-overlay-time-status-renderer><badge-shape class="badge-shape-wiz">${song.duration}</badge-shape></ytm-thumbnail-overlay-time-status-renderer>
+                                </div>
+                            </ytm-compact-thumbnail>
+                        </a>
+                        <div class="compact-media-item-metadata">
+                            <a href="/songs?v=${song.videoId}" class="compact-media-item-metadata-content">
+                                <h4 class="compact-media-item-headline">${song.songName}</h4>
+                                <div class="subhead"><span class="yt-core-attributed-string">${song.fullName} ${song.location ? `đến từ ${song.location}` : ""}</span></div>
+                            </a>
                         </div>
-                      </ytm-compact-thumbnail>
-                    </a>
-                    <div class="compact-media-item-metadata" data-has-badges="false">
-                      <a href="/songs?v=${song.videoId}" class="compact-media-item-metadata-content">
-                        <h4 class="compact-media-item-headline" style="">
-                          <span class="yt-core-attributed-string yt-core-attributed-string--link-inherit-color" aria-label="${song.songName} của ${song.channelTitle} ${durationText}" role="text">${song.songName}</span>
-                        </h4>
-                        <div class="subhead" aria-hidden="true" style="">
-                          <div class="compact-media-item-byline small-text">
-                            <span class="yt-core-attributed-string">${song.fullName} ${song.location ? `đến từ ${song.location}` : ""}</span>
-                          </div>
-                        </div>
-                      </a>
                     </div>
-                  </div>
                 </ytm-playlist-panel-video-renderer>
             `;
             playlistContainer.innerHTML += songElement;
@@ -208,186 +146,92 @@ function displaySongs(userIndexes, songs, currentUserId) {
     });
 }
 
-// Khởi tạo player khi chưa tồn tại, hoặc chuyển sang video ID mới khi cần
+function initializeVideoPlayer() {
+    isVideoPlayerInitialized = true;
+    initializeOrPlayVideo();
+}
+
 function initializeOrPlayVideo(videoId = null) {
-  if (!player) {
-    // Tạo player mới nếu chưa tồn tại
     const initialVideoId = videoId || getRandomVideoId();
     player = new YT.Player("player", {
-      videoId: initialVideoId,
-      playerVars: {
-        autoplay: 1,
-        controls: shouldShowControls() ? 1 : 0,
-        rel: 0,
-        iv_load_policy: 3,
-        mute: shouldShowControls() ? 0 : 1,
-        playsinline: 1,
-        enablejsapi: 1,
-        modestbranding: 1,
-        showinfo: 0,
-        wmode: 'transparent',
-      },
-      events: {
-        onReady: onPlayerReady,
-        onStateChange: onPlayerStateChange,
-      }
+        videoId: initialVideoId,
+        playerVars: { autoplay: 1, controls: shouldShowControls() ? 1 : 0, mute: !shouldShowControls() },
+        events: { onReady: onPlayerReady, onStateChange: onPlayerStateChange }
     });
-  } else if (videoId) {
-    player.loadVideoById(videoId);
-  }
 }
 
-// Lấy video ngẫu nhiên từ danh sách
 function getRandomVideoId() {
-  const randomIndex = Math.floor(Math.random() * initialVideoIds.length);
-  return initialVideoIds[randomIndex];
+    return initialVideoIds[Math.floor(Math.random() * initialVideoIds.length)];
 }
 
-// Kiểm tra kích thước màn hình để hiển thị controls phù hợp
 function shouldShowControls() {
-  return $(window).width() >= 768;
+    return $(window).width() >= 768;
 }
 
-// Hàm xử lý khi player đã sẵn sàng
 function onPlayerReady(event) {
-  event.target.setPlaybackQuality('highres');
-  startUpdatingVideoData();
+    event.target.setPlaybackQuality('highres');
+    startUpdatingVideoData();
 }
 
-// Hàm cập nhật video data vào Realtime Database
-function updateVideoData() {
-  if (!player) return;
-  const dbRef = ref(getDatabase(), 'videoStatus');
-  const playerState = player.getPlayerState();
-  const currentTime = Math.floor(player.getCurrentTime());
-  if (!isNaN(currentTime)) {
-    update(dbRef, {
-      currentVideoId: currentVideo ? currentVideo.videoId : null,
-      nextVideoId: nextVideo ? nextVideo.videoId : null,
-      status: playerState === YT.PlayerState.PLAYING ? 'play' : 'pause',
-      currentTime: currentTime,
-      volume: player.getVolume()
-    }).catch(error => console.error("Error updating video data:", error));
-  }
-}
-
-// Bắt đầu cập nhật video data liên tục
 function startUpdatingVideoData() {
-  isUpdating = true;
-  const update = () => {
-    if (isUpdating) {
-      updateVideoData();
-      requestAnimationFrame(update);
-    }
-  };
-  requestAnimationFrame(update);
-}
-
-// Dừng cập nhật video data
-function stopUpdatingVideoData() {
-  isUpdating = false;
-}
-
-// Xử lý khi trạng thái video thay đổi
-function onPlayerStateChange(event) {
-  switch (event.data) {
-    case YT.PlayerState.ENDED:
-      handleVideoEnd();
-      break;
-    case YT.PlayerState.PLAYING:
-      if (!isUpdating) {
-        startUpdatingVideoData();
-      }
-      break;
-    default:
-      stopUpdatingVideoData();
-      break;
-  }
-}
-
-// Xử lý khi video kết thúc
-function handleVideoEnd() {
-  const currentVideoIdAPI = player?.getVideoData().video_id;
-  if (!currentVideoIdAPI || initialVideoIds.includes(currentVideoIdAPI)) {
-    playRandomVideo();
-    return;
-  }
-
-  const currentUserId = customerData.find(video => video.videoId === currentVideoIdAPI)?.customerId;
-  if (currentUserId) {
-    handleEndOfVideo(currentUserId);
-    updateUserStatus(currentUserId, nextVideo?.customerId);
-    initializeOrPlayVideo(nextVideo?.videoId || getRandomVideoId());
-  } else {
-    playRandomVideo();
-  }
-}
-
-// Phát video ngẫu nhiên
-function playRandomVideo() {
-  initializeOrPlayVideo(getRandomVideoId());
-}
-
-// Theo dõi thay đổi video từ Realtime Database
-function monitorVideoStatusChanges() {
-  const dbRef = ref(getDatabase(), 'videoStatus/currentVideoId');
-  onValue(dbRef, snapshot => {
-    const newVideoId = snapshot.val();
-    if (newVideoId && player.getVideoData().video_id !== newVideoId) {
-      currentVideo = customerData.find(video => video.videoId === newVideoId);
-      initializeOrPlayVideo(newVideoId);
-    }
-  }, error => console.error("Error monitoring video status changes:", error));
-}
-
-// Cập nhật trạng thái người dùng trong Realtime Database
-function updateUserStatus(currentUserId, nextUserId) {
-  if (currentUserId === lastUpdatedUserId) return;
-  const dbRef = ref(getDatabase(), 'users');
-  const updates = {
-    [`${currentUserId}/played`]: true,
-    [`${currentUserId}/select`]: false,
-    [`${currentUserId}/priority`]: false
-  };
-  if (nextUserId) {
-    updates[`${nextUserId}/select`] = true;
-  }
-  update(dbRef, updates)
-    .then(() => lastUpdatedUserId = currentUserId)
-    .catch(error => console.error("Error updating user status:", error));
-}
-
-monitorVideoStatusChanges();
-
-$(document).ready(function() {
-  $(document).on('click', 'lazy-list a', function(event) {
-    event.preventDefault();
-    const parentRenderer = $(this).closest('.ytm-playlist-panel-video-renderer-v2');
-    const videoId = parentRenderer.data('video-id');
-    const userId = parentRenderer.data('user-id');
-
-    if (parentRenderer.hasClass('ytm-playlist-panel-video-renderer-v2--selected')) {
-      replaySong(videoId);
-    } else {
-      const updates = {};
-      updates[`users/${userId}/priority`] = true;
-      const usersRef = ref(realTimeDb, 'users');
-      onValue(usersRef, (snapshot) => {
-        if (snapshot.exists()) {
-          const usersData = snapshot.val();
-          Object.keys(usersData).forEach((key) => {
-            if (key !== userId) {
-              updates[`users/${key}/priority`] = false;
-            }
-          });
-          update(ref(realTimeDb), updates)
-            .then(() => {
-            })
-            .catch((error) => {
-              console.error(`Lỗi cập nhật dữ liệu: ${error}`);
-            });
+    isUpdating = true;
+    requestAnimationFrame(function update() {
+        if (isUpdating) {
+            updateVideoData();
+            requestAnimationFrame(update);
         }
-      }, { onlyOnce: true });
+    });
+}
+
+function updateVideoData() {
+    const dbRef = ref(getDatabase(), 'videoStatus');
+    const playerState = player.getPlayerState();
+    const currentTime = Math.floor(player.getCurrentTime());
+
+    if (!isNaN(currentTime)) {
+        update(dbRef, {
+            currentVideoId: currentVideo ? currentVideo.videoId : null,
+            nextVideoId: nextVideo ? nextVideo.videoId : null,
+            status: playerState === YT.PlayerState.PLAYING ? 'play' : 'pause',
+            currentTime: currentTime,
+            volume: player.getVolume()
+        }).catch(error => console.error("Error updating video data:", error));
     }
-  });
-});
+}
+
+function stopUpdatingVideoData() {
+    isUpdating = false;
+}
+
+function onPlayerStateChange(event) {
+    if (event.data === YT.PlayerState.ENDED) handleVideoEnd();
+    if (event.data === YT.PlayerState.PLAYING && !isUpdating) startUpdatingVideoData();
+    else stopUpdatingVideoData();
+}
+
+function handleVideoEnd() {
+    const currentVideoIdAPI = player?.getVideoData().video_id;
+    if (!currentVideoIdAPI || initialVideoIds.includes(currentVideoIdAPI)) {
+        playRandomVideo();
+        return;
+    }
+
+    if (currentVideo) {
+        handleEndOfVideo(currentVideo.customerId)
+            .then(() => {
+                currentVideo = nextVideo;
+                nextVideo = customerData[customerData.findIndex(c => c.customerId === nextVideo.customerId) + 1];
+                playNextVideo();
+            })
+            .catch(error => console.error("Error handling end of video:", error));
+    } else playRandomVideo();
+}
+
+function playNextVideo() {
+    player.loadVideoById(currentVideo.videoId);
+}
+
+function playRandomVideo() {
+    const randomVideoId = getRandomVideoId();
+    player.loadVideoById(randomVideoId);
+}
