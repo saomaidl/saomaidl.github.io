e635966d8444316b6a55829a9d73eebe1ee6a2aa
saomai <cumgar.dl47@gmail.com>
2024-11-19 20:52:12 +0700

    Update sort.js

--- assets/js/sort.js
+++ assets/js/sort.js
@@ -226,7 +226,7 @@ function createYouTubePlayer(videoId) {
       controls: showControls ? 0 : 0,
       rel: 0,
       iv_load_policy: 3,
-      mute: showControls ? 1 : 1,
+      mute: showControls ? 0 : 1,
       playsinline: 1,
       enablejsapi: 1,
       modestbranding: 1,
