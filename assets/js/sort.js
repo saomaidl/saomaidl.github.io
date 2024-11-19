28c81a5e0f2c3766e45faa23ca39672a4a5e88b1
saomai <cumgar.dl47@gmail.com>
2024-11-19 21:41:54 +0700

    Update sort.js

--- assets/js/sort.js
+++ assets/js/sort.js
@@ -249,10 +249,10 @@ function onPlayerReady(event) {
         const playPauseIcon = $(this).find('path');
         if (playerState === YT.PlayerState.PLAYING) {
             player.pauseVideo();
-            playPauseIcon.attr('d', 'M9 19H7V5h2Zm8-14h-2v14h2Z');
+            playPauseIcon.attr('d', 'm7 4 12 8-12 8V4z');
         } else {
             player.playVideo();
-            playPauseIcon.attr('d', 'm7 4 12 8-12 8V4z');
+            playPauseIcon.attr('d', 'M9 19H7V5h2Zm8-14h-2v14h2Z');
         }
     });
 }
